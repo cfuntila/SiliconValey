1670702d0560248b8ee236d348f4f8f2a4195061
cfuntila <cfuntila@ucsc.edu>
2020-02-28 15:08:01 -0800

    Initial Commit

---
+++ SiliconValley/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiliconValley
+//
+//  Created by Charity Funtila on 2/28/20.
+//  Copyright © 2020 Charity Funtila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
